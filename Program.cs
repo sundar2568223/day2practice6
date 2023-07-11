825e92093b4484f2f50e10408e248c55c29e525f
sunder2568223 <sunderyssr@gmail.com>
2023-07-11 12:46:21 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Consoleday2prac4
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            DateTime matchDate = new DateTime(day: 13, month: 07, year: 2023);
+
+            DateTime currentDate = DateTime.Now;
+
+            TimeSpan ts = matchDate.Subtract(currentDate);
+
+            Console.WriteLine("Days Remain " + ts.Days);
+
+            Console.WriteLine("Days Remain " + ts.Hours);
+
+            Console.WriteLine(ts.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
